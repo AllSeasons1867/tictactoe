1030ade092a0a77c6a2649d80dafede177a7877a
FW <35238548+AllSeasons1867@users.noreply.github.com>
2023-09-13 12:56:19 -0600

    added board display

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,15 +1,25 @@
-gameBoard = [["","",""],["","",""],["","",""]]
+$gameBoard = [[" "," "," "],[" "," "," "],[" "," "," "]]
 
 def getComputerMove
 
 end
 
 def playerMove
-
+    puts "Choose where X goes"
+    move = gets.chomp
+    return move
 end
 
 def gameStart
-    playerMove = getPlayerMove
-    computerMove = getComputerMove 
+    # playerMove = getPlayerMove
+    # computerMove = getComputerMove 
+    puts "#{$gameBoard[0][0]} | #{$gameBoard[0][1]} | #{$gameBoard[0][2]}"
+    puts "#{$gameBoard[1][0]} | #{$gameBoard[1][1]} | #{$gameBoard[1][2]}"
+    puts "#{$gameBoard[2][0]} | #{$gameBoard[2][1]} | #{$gameBoard[2][2]}"
 end
+
+gameStart
+
+
+
 
